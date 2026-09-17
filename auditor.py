5bd78f0dbfd36974bbec4d227bf7d93572db31dc
Thach Ke Vin <2604693@sit.singaporetech.edu.sg>
2026-09-17 21:39:34 +0800

    Added While True loop and increment inventory, exceeds max inventory handling

--- auditor.py
+++ auditor.py
@@ -5,3 +5,26 @@ def inventory_function():
     units_unprocessed = 0
     max_inventory = 500
 
+    while True:
+            # ask for input
+            response = input(f"Current inventory: {inventory}. Please enter your stock quantity to add to the inventory: ").lower().strip()
+
+            # if input is quit then print relevant info, and then break While loop
+            if response =="quit":
+                break
+
+            # if input is valid non negative integer then add to inventory
+            elif response.isdigit():
+                inventory += int(response)
+
+                # if inventory doesnt exceed max inventory then increment inventory with the user response
+                if inventory <= max_inventory:
+                    print(f"You have successfully added {response} to the inventory")
+
+                # if inventory exceeeds max inventory, then set inventory to max and break While loop
+                else:
+                    units_unprocessed = inventory - max_inventory # calculates leftover units
+                    inventory = max_inventory # assume units will be added till inventory is maxed, meaning left over units that are not added
+                    failed_entries += 1 # considered failed entry since not all units were added successfully
+                    print(f"ALERT: Maximum {max_inventory} units in inventory exceeded. Stopping...")
+                break
